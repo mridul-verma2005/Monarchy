cbd6bee6247536588c2d26a1e5d0f8217320f3dd
Mridul Verma <mridulverma@gmail.com>
2026-05-25 19:41:43 +0530

    added a serial port for logging , some fuction that will run before the main mainly to check the stack and the searil port for logging text and cursor now have there simple function for printing and logging have a simple log function

--- os/.asm_files/loader.asm
+++ os/.asm_files/loader.asm
@@ -1,7 +1,10 @@
 global loader
 extern main
-extern write_to_screen
+extern write_to_log
+extern serial_port_check
+COM1 equ 0x3F8
 
+section .multiboot
 MAGIC_NUMBER equ 0x1BADB002
 FLAGS equ 0x0
 CHECKSUM equ -(MAGIC_NUMBER)
@@ -13,8 +16,8 @@ section .bss
         resb KERNEL_STACK_SIZE
 
 section .rodata
-    stack_init db "Stack initialized",0  
-    stact_err db "Error in Stack initialization",0
+    stack_init db "Stack initialized at 0x00104000",0  
+    stack_err db "Error in Stack initialization aborting loading kernel",0
 
 section .multiboot
     ; db 0x0
@@ -25,14 +28,19 @@ section .multiboot
 
 section .text
 loader:
+    call serial_port_check
+    cmp eax , 0
+    jne serial_port_error
     mov esp , kernel_stack + KERNEL_STACK_SIZE
     mov ebx , 0xCAFEBABE
     cmp esp , 0x00104000
     jne stact_err_print
     stack_init_print:
+        push dword COM1
+        push dword 0x0
         push dword stack_init
-        call write_to_screen
-        add esp , 4
+        call write_to_log
+        add esp , 16
 
 
 
@@ -42,11 +50,12 @@ loader:
         jmp .loop
 
 
-stact_err_print:
-        push dword stact_err
-        call write_to_screen
-        add esp , 4
+stact_err_print:    // cause if the stack is not set correcly then how you can even push var on it 
         .err_loop:
             hlt
             jmp .err_loop
 
+
+serial_port_error:
+    hlt
+    jmp serial_port_error;
